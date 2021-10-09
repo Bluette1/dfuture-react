378bbbb87443b00c3d2bc6d9e60dc4f671e83415
Bluette1 <marylene.sawyer@gmail.com>
2021-10-09 15:18:37 +0200

    Add the upload form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import axios from "axios";
 
-function App() {
+const App = () => {
+  const [name, setName] = useState("");
+  const [clientName, setClientName] = useState("");
+  const [file, setFile] = useState(null);
+
+  const handleNameChange = (e) => {
+    setName(e.target.value);
+  };
+
+  const handleClientNameChange = (e) => {
+    setClientName(e.target.value);
+  };
+
+  const handleFileChange = (e) => {
+    setFile(e.target.files[0]);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let form_data = new FormData();
+    form_data.append("file", file, file.name);
+    form_data.append("name", name);
+    form_data.append("client_name", clientName);
+    let url = "http://localhost:8000/api/documents/";
+    axios
+      .post(url, form_data, {
+        headers: {
+          "content-type": "multipart/form-data",
+        },
+      })
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((err) => console.log(err));
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <form onSubmit={handleSubmit}>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <input
+            type="text"
+            placeholder="Name"
+            value={name}
+            onChange={handleNameChange}
+            required
+          />
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <p>
+          <input
+            type="text"
+            placeholder="Your Name"
+            value={clientName}
+            onChange={handleClientNameChange}
+            required
+          />
+        </p>
+        <p>
+          <input
+            type="file"
+            accept="image/*,.pdf, .doc, .rtf, .docx"
+            onChange={handleFileChange}
+            required
+          />
+        </p>
+        <input type="submit" />
+      </form>
     </div>
   );
-}
-
+  // }
+};
 export default App;
