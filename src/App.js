995e1a20158fd4b8db3b57939a09f5a0af3e4b5c
Bluette1 <marylene.sawyer@gmail.com>
2021-10-13 18:33:21 +0200

    Add link to switch between Client and RM dashboards

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
-import { Router, Switch, Route } from "react-router-dom";
+import { Router, Switch, Route, Link } from "react-router-dom";
+import React, { useState } from "react";
 import ClientDashboard from "./components/ClientDashboard";
 import RMDashboard from "./components/RMDashboard";
+import SwitchDashboards from "./components/SwitchDashBoards";
 import { createBrowserHistory } from 'history';
 
 const history = createBrowserHistory();
+
 const App = () => {
+  const [showRMDashboard, setShowRMDashboard] = useState(true);
+
+  const currentPath = showRMDashboard ? '/client' : '/'
+  const currentDashboard = showRMDashboard ? 'Switch to Client Dashboard' : 'Switch to Home Dashboard'
+  const handleClick = () => {
+    setShowRMDashboard(!showRMDashboard);
+  }
   return (
     <Router history={history}>
       <div>
         <nav className="navbar navbar-expand-lg navbar-dark bg-navbar d-flex justify-content-around justify-content-lg-between">
         <h2>DFuture Web App</h2>
+        <Link to={currentPath} onClick={handleClick}>{currentDashboard}</Link>
         </nav>
         <div>
           <Switch>
